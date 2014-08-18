9bae6320b9e75dc4c87946865ac0a74eea239da2
curtis <curtis@serverascode.com>
2014-08-18 14:18:11 -0600

    moving to if name is main

--- nova.py
+++ nova.py
@@ -11,7 +11,7 @@ try:
 except:
     import simplejson as json
 
-def nova_load_config_file():
+def get_config():
     p = ConfigParser.SafeConfigParser()
     path1 = os.getcwd() + "/nova.ini"
     path2 = os.path.expanduser(os.environ.get('ANSIBLE_CONFIG', "~/nova.ini"))
@@ -37,71 +37,75 @@ def add_meta(vm_name, inventory, group, ip):
         inventory['_meta']['hostvars'][vm_name] = {}
         inventory['_meta']['hostvars'][vm_name]['ansible_ssh_host'] = ip
 
-################################################################################
-#
-# MAIN
-#
-################################################################################
+def get_inventory(client, inventory):
 
-config = nova_load_config_file()
+    groups = [ 'undefined']
 
-client = nova_client.Client(
-    version     = config.get('openstack', 'version'),
-    username    = config.get('openstack', 'username'),
-    api_key     = config.get('openstack', 'api_key'),
-    auth_url    = config.get('openstack', 'auth_url'),
-    region_name = config.get('openstack', 'region_name'),
-    project_id  = config.get('openstack', 'project_id'),
-    auth_system = config.get('openstack', 'auth_system')
-)
-
-groups = [ 'undefined']
-inventory = {}
-inventory['_meta'] = {}
-inventory['_meta']['hostvars'] = {}
-
-for group in groups:
-    if not group in inventory:
+    for group in groups:
+      if not group in inventory:
         inventory[group] = {
           'hosts' : [],
         }
 
-if len(sys.argv) == 2 and (sys.argv[1] == '--list'):
-    groups = {}
-
-    # Cycle on servers
     for f in client.servers.list():
 
+      groups = {}
+
       private = [ x['addr'] for x in getattr(f, 'addresses').itervalues().next()
-       if x['OS-EXT-IPS:type'] == 'fixed' ]
+         if x['OS-EXT-IPS:type'] == 'fixed' ]
 
       public  = [ x['addr'] for x in getattr(f, 'addresses').itervalues().next()
-        if x['OS-EXT-IPS:type'] == 'floating']
+          if x['OS-EXT-IPS:type'] == 'floating']
 
-      # Define group (or set to empty string)
+        # Define group (or set to empty string)
       group = f.metadata['group'] if f.metadata.has_key('group') else 'undefined'
 
-      # Create group if not exist
+        # Create group if not exist
       if group not in groups:
-          groups[group] = []
+        groups[group] = []
 
       if public:
-          # just take the first ip
-          ip = public[0]
-          if len(ip) < 16:
-            add_meta(f.name, inventory, group, ip)
-            continue
+        # just take the first ip
+        ip = public[0]
+        if len(ip) < 16:
+          add_meta(f.name, inventory, group, ip)
+      elif private:
+        ip = private[0]
+        if len(ip) < 16:
+          add_meta(f.name, inventory, group, ip)
 
-      if private:
-          ip = private[0]
-          if len(ip) < 16:
-            add_meta(f.name, inventory, group, ip)
-            continue
+      # Return server list
+    return inventory
 
-    # Return server list
-    print json.dumps(inventory)
-    sys.exit(0)
 
-else:
-    print "usage: --list"
-    sys.exit(1)
+def main(args):
+  config = get_config()
+
+  client = nova_client.Client(
+      version     = config.get('openstack', 'version'),
+      username    = config.get('openstack', 'username'),
+      api_key     = config.get('openstack', 'api_key'),
+      auth_url    = config.get('openstack', 'auth_url'),
+      region_name = config.get('openstack', 'region_name'),
+      project_id  = config.get('openstack', 'project_id'),
+      auth_system = config.get('openstack', 'auth_system')
+  )
+
+  # FIXME: Where to be getting groups from? Some examples show from openstack
+  #        metadata
+
+  inventory = {}
+  inventory['_meta'] = {}
+  inventory['_meta']['hostvars'] = {}
+
+
+  if len(sys.argv) == 2 and (sys.argv[1] == '--list'):
+    inventory = get_inventory(client, inventory )
+    if inventory:
+      print json.dumps(inventory)
+  else:
+      print "usage: --list"
+      sys.exit(1)
+
+if __name__ == "__main__":
+    main(sys.argv)
